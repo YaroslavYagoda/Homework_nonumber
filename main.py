bbe6a7cb212c5638159ddae2692353f1d286fce5
Ярослав <Yagavdv14@yandex.ru>
2024-09-21 08:04:06 +0300

    не смог реализовать снятие денег без использования метода напрямую

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
 """
-Создайте базовый класс `Shape` с методом `area`, который будет возвращать площадь фигуры.
-Затем создайте два подкласса `Rectangle` и `Circle`,
-которые наследуют от `Shape` и переопределяют метод `area` для расчета площади прямоугольника и круга соответственно.
-Создайте объекты этих классов и вызовите метод `area` для каждого из них.
+Создайте класс BankAccount с приватным атрибутом balance. Реализуйте методы для депозита, снятия и проверки баланса.
+Используйте методы доступа для работы с приватным атрибутом.
+Подсказка:
+Для выполнения этой задачи используйте принципы инкапсуляции.
+deposit money
+withdraw money
+check the balance
 """
-from math import pi
 
 
-class Shape:
-    def get_area(self):
-        pass
+class BankAccount:
+    def __init__(self):
+        self.__balance = 0
+
+    def deposit_money(self, money: float):
+        if money <= 0:
+            print('Недопустимая операция')
+        else:
+            self.__balance += money
+
+    def withdraw_money(self, money: float):
+        if money > self.__balance:
+            print('Недостаточно средств!')
+        elif money <= 0:
+            print('Недопустимая операция')
+        else:
+            self.__balance -= money
+
+    def print_balance(self):
+        return self.__balance
+
+    balance = property(print_balance, deposit_money, withdraw_money, 'Разрешения для счета')
 
 
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def get_area(self):
-        return self.width * self.height
-
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def get_area(self):
-        return pi * self.radius ** 2
-
-
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
-
-    def get_area(self):
-        return self.base * self.height * 0.5
-
-
-cl = Rectangle(2, 3)
-print(cl.get_area())
-cl = Circle(5)
-print(cl.get_area())
-cl = Triangle(5, 6)
-print(cl.get_area())
+cash_account = BankAccount()
+cash_account.print_balance()
+cash_account.deposit_money(100)
+cash_account.withdraw_money(10.1)
+print(cash_account.print_balance())
+cash_account.balance = 20
+print(cash_account.balance)
+cash_account.balance = -10
+print(cash_account.balance)
